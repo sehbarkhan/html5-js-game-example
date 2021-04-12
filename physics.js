9e613177064ba461b5c9dc59cef7f64196404608
sehbarkhan <md.sehbar@gmail.com>
2021-04-13 02:56:32 +0530

    modified code.html and added physics.js file

--- physics.js
+++ physics.js
@@ -1,19 +1,3 @@
-
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #f1f1f1;
-}
-</style>
-</head>
-<body onload="startGame()">
-<script>
-
 var myGamePiece;
 var myObstacles = [];
 var myScore;
@@ -130,8 +114,3 @@ function everyinterval(n) {
 function accelerate(n) {
     myGamePiece.gravity = n;
 }
-</script>
-<br>
-<button onmousedown="accelerate(-0.2)" onmouseup="accelerate(0.05)">Jump in Air</button>
-</body>
-</html>
